df13a5ed6345b3ed9f0bdb5c4e89ccdcd129bbc3
roberto <roberto.junior@cfmv.gov.br>
2023-08-30 14:12:50 -0300

    .

--- src/main/kotlin/com/cafeteria/routes/LoginRouting.kt
+++ src/main/kotlin/com/cafeteria/routes/LoginRouting.kt
@@ -21,36 +21,43 @@ fun Route.loginRouting(database: MongoDatabase) {
     route("/login") {
         val collection = database.getCollection<User>("users")
         post {
-            val user = call.receive<User>()
 
-            val token = JWT.create()
-                .withAudience(System.getenv("audience"))
-                .withIssuer(System.getenv("issuer"))
+            try {
+                val user = call.receive<User>()
+
+                val token = JWT.create()
+                    .withAudience(System.getenv("audience"))
+                    .withIssuer(System.getenv("issuer"))
 //                .withClaim("email", user.email)
-                .withExpiresAt(Date(System.currentTimeMillis() + 600000))
-                .withPayload(mapOf("email" to user.email))
-                .sign(Algorithm.HMAC256(System.getenv("secret")))
+                    .withExpiresAt(Date(System.currentTimeMillis() + 600000))
+                    .withPayload(mapOf("email" to user.email))
+                    .sign(Algorithm.HMAC256(System.getenv("secret")))
 
-            val validUser = collection.find(eq("email", user.email))
+                val validUser = collection.find(eq("email", user.email))
 
-            if (validUser.toList().isNotEmpty()) {
-                if (validUser.first().senha == user.senha) {
-                    call.respond(
-                        hashMapOf("token" to token, "email" to user.email)
-                    )
+                if (validUser.toList().isNotEmpty()) {
+                    if (validUser.first().senha == user.senha) {
+                        call.respond(
+                            hashMapOf("token" to token, "email" to user.email)
+                        )
+                    } else {
+                        call.respondText(
+                            "Senha incorreta",
+                            status = HttpStatusCode.NotFound
+                        )
+                    }
                 } else {
                     call.respondText(
-                        "Senha incorreta",
+                        "Usuario não encontrado",
                         status = HttpStatusCode.NotFound
                     )
                 }
-            } else {
-                call.respondText(
-                    "Usuario não encontrado",
-                    status = HttpStatusCode.NotFound
-                )
+
+            } catch (e: Exception) {
+                call.respondText("${e.message}")
             }
         }
+
     }
     route("/getUsers") {
         authenticate {
